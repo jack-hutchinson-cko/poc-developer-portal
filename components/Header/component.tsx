1c47f87e59ab41ad3fe4cda89eedc921faa50333
Jack Hutchinson <jack.hutchinson@checkout.com>
2022-04-12 19:08:10 +0100

    No theme switcher

--- components/Header/component.tsx
+++ components/Header/component.tsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import { ThemeToggle } from '../index';
-import LoginLink from './LoginLink';
-import MenuLink from './MenuLink';
+import React from "react";
+import { ThemeToggle } from "../index";
+import LoginLink from "./LoginLink";
+import MenuLink from "./MenuLink";
 import {
-	StyledContainer,
-	StyledLink,
-	StyledLeftHeader,
-	StyledLogo,
-	StyledLogoIcon,
-	StyledLogoText,
-	StyledRightHeader,
-	StyledSearchBox,
-} from './styles';
+  StyledContainer,
+  StyledLink,
+  StyledLeftHeader,
+  StyledLogo,
+  StyledLogoIcon,
+  StyledLogoText,
+  StyledRightHeader,
+  StyledSearchBox,
+} from "./styles";
 
 const Header = (): JSX.Element => (
-	<StyledContainer>
-		<StyledLeftHeader>
-			<StyledLogo href="/developer-portal/reference/overview/">
-				<StyledLogoIcon />
-				<StyledLogoText>Documentation</StyledLogoText>
-			</StyledLogo>
-			<MenuLink />
-			<StyledSearchBox />
-		</StyledLeftHeader>
-		<StyledRightHeader>
-			<LoginLink />
-			<StyledLink>Get test account</StyledLink>
-			<ThemeToggle />
-		</StyledRightHeader>
-	</StyledContainer>
+  <StyledContainer>
+    <StyledLeftHeader>
+      <StyledLogo href="/developer-portal/reference/overview/">
+        <StyledLogoIcon />
+        <StyledLogoText>Documentation</StyledLogoText>
+      </StyledLogo>
+      <MenuLink />
+      <StyledSearchBox />
+    </StyledLeftHeader>
+    <StyledRightHeader>
+      <LoginLink />
+      <StyledLink>Get test account</StyledLink>
+      {/* <ThemeToggle /> */}
+    </StyledRightHeader>
+  </StyledContainer>
 );
 
 export default Header;
